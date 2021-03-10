ce91ecb4d25f2ad3941c48ce178fa191f87e449a
marine-vdb <marinewambergue@gmail.com>
2021-03-10 14:23:01 +0100

    Ajout des sous catégories dans nav.php

--- nav.php
+++ nav.php
@@ -15,9 +15,9 @@
                     Marques
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="#">Janod</a>
+                    <a class="dropdown-item" href="#">Mini-Kane</a>
+                    <a class="dropdown-item" href="#">Moulin Roty</a>
                     </div>
                 </li>
 
@@ -27,9 +27,16 @@
                     Puériculture
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="#">Bavoirs et Langes</a>
+                    <a class="dropdown-item" href="#">Couvertures et plaids</a>
+                    <a class="dropdown-item" href="#">Linge de lit</a>
+                    <a class="dropdown-item" href="#">Protège-carnets de santé</a>
+                    <a class="dropdown-item" href="#">Repas</a>
+                    <a class="dropdown-item" href="#">Sacs à langer, Sacs week-end</a>
+                    <a class="dropdown-item" href="#">Sortie de bain</a>
+                    <a class="dropdown-item" href="#">Tétines et attaches-tétines</a>
+                    <a class="dropdown-item" href="#">Toilette et soins de bébé</a>
+                   
                     </div>
                 </li>
 
@@ -39,23 +46,26 @@
                     Eveil et Jeux
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="#">Draisiennes, Tricycles</a>
+                    <a class="dropdown-item" href="#">Hochets, Anneaux de dentition</a>
+                    <a class="dropdown-item" href="#">Garages, Voitures</a>
+                    <a class="dropdown-item" href="#">Instruments de musique</a>
+                    <a class="dropdown-item" href="#">Jeux sensoriels</a>
+                    <a class="dropdown-item" href="#">Jeux éducatifs, jeux de société, puzzles</a>
+                    <a class="dropdown-item" href="#">Jeux d'imitation</a>
+                    <a class="dropdown-item" href="#">Jeux de bain</a>
+                    <a class="dropdown-item" href="#">Jouets en bois</a>
+                    <a class="dropdown-item" href="#">Livres</a>
+                    <a class="dropdown-item" href="#">Loisirs créatifs</a>
+                    <a class="dropdown-item" href="#">Poupées, Poupons</a>
+                    <a class="dropdown-item" href="#">Porteurs, chariots et bascules</a>
                     </div>
                 </li>
 
 
                 <!-- Catégorie 4 -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Peluches et Doudous
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Peluches et doudous</a>
                 </li>
 
                 <!-- Catégorie 5 -->
@@ -64,9 +74,13 @@
                     Décoration
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="#">Boîtes à musique, Mobiles</a>
+                    <a class="dropdown-item" href="#">Coussins</a>
+                    <a class="dropdown-item" href="#">Décoration murale</a>
+                    <a class="dropdown-item" href="#">Petit mobilier</a>
+                    <a class="dropdown-item" href="#">Rangement</a>
+                    <a class="dropdown-item" href="#">Tapis</a>
+                    <a class="dropdown-item" href="#">Veilleuses, Luminaires</a>
                     </div>
                 </li>
 
@@ -76,9 +90,13 @@
                     Mode et Accessoires
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="#">Accessoires</a>
+                    <a class="dropdown-item" href="#">Accessoires cheveux</a>
+                    <a class="dropdown-item" href="#">Beauté, Maquillage</a>
+                    <a class="dropdown-item" href="#">Bijoux</a>
+                    <a class="dropdown-item" href="#">Chaussons</a>
+                    <a class="dropdown-item" href="#">Lunettes de soleil</a>
+                    <a class="dropdown-item" href="#">Sacs, Cartables, trousses</a>
                     </div>
                 </li>
 
