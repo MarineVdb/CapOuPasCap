69e5efa19d3b025b8ae330051487d74295db5f89
marine-vdb <marinewambergue@gmail.com>
2021-03-20 13:23:12 +0100

    Ajout du fichier produit.php

---
+++ public/produit.php
@@ -0,0 +1,29 @@
+<?php include '../header.php';?> 
+
+
+<?php 
+if(isset($_GET) && isset($_GET['id'])){
+    $id = intval($_GET['id']);
+    include '../src/connexionBdd.php';
+    $sql = "SELECT produit_id, nom, `description`, prix FROM produit WHERE produit_id='".$id."'" ;
+    //J'envoie la requête à la BDD et je l'enregistre dans une variable
+    $reponse = $bdd ->query($sql);
+    //Si on trouve un ID, on affiche le nom
+        if ($donnees = $reponse->fetch()){
+            //echo 'Ce produit existe';
+            echo $donnees['produit_id'];
+            echo $donnees['nom'];
+            echo $donnees['description'];
+
+        } else {
+            echo 'ce produit n\'existe pas';
+        }
+} else {
+    echo 'erreur 404';
+}
+
+
+
+?>
+
+<?php include '../footer.php';?> 
