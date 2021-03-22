4689a36e096bca320f275566a810998ec18412e0
ratarby <ratarby@gmail.com>
2021-03-22 14:34:37 +0100

    Création du fichier QuiSommesNous.php

---
+++ public/QuiSommesNous.php
@@ -0,0 +1,20 @@
+<h2 style="text-align: center" class="Bonjour">Bonjour et Bienvenue sur le site</h2></br>
+<h1 style="text-align:center" class="CapoupasCap">Cap ou pas Cap<h1>
+<p style="text-align:justify">Voyagez parmi plusieurs univers, retombez en enfance, cassez les codes, proposez des objets qui feront l'unanimité chez les petits comme les grands, voilà ce qui nous anime au quotidien.<p> 
+
+<p style="text-align:justify">Le concept  Cap ou pas Cap a fleuri à Cambrai. Depuis nous grandissons avec l'envie de partager avec vous toutes les plus jolies choses de l’univers de l'enfant. Nous mettons à l’honneur les pièces qui vous feront retomber en enfance.</p>
+<p style="text-align:justify">Cap ou pas Cap c’est avant tout un voyage parmi mille couleurs dans différents univers. Cadeaux de naissance, gadgets décalés, jouets, vaisselle pour enfants et objets de décoration uniques : nous avons des idées plein les tiroirs !</p>
+
+<p style="text-align:justify">Ce joyeux bazar vous réserve des surprises pétillantes !</p>
+
+<p style="text-align:justify">A l’affut des tendances, nous sélectionnons de grandes marques, mais aussi des créateurs pour vous proposer des exclusivités. Les marques sont toutes choisies pour leur originalité, leur qualité ou leur savoir-faire. </p>
+
+<p style="text-align:justify">Nous nous appuyons également sur une talentueuse équipe en interne pour dénicher, sélectionner  de merveilleux produits  qui sont exclusivement présentes dans nos magasins Le Cap ou pas Cap.</p>
+
+<p style="text-align:justify">Les produits sont sélectionnés & conçus pour s’inscrire dans l’ADN de Cap ou pas Cap.</p>
+
+<p style="text-align:justify">Cap ou pas Cap vous invite à un véritable voyage des genres et à une séance shopping haute en couleurs ! Venez vite nous rencontrer!</p>
+
+<p style="text-align:justify">Rejoindre Le Cap ou pas Cap, c’est participer à une jolie aventure humaine.</p>
+
+<p style="text-align:justify">Si ça vous tente, n’hésitez plus.</p>
